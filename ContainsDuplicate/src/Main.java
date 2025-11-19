51a81e7519cb8f6186d069a51c615b0d660ac105
Amr Kahla <amrkahla6@gmail.com>
2025-11-19 19:53:58 +0200

    217. Contains Duplicate

--- ContainsDuplicate/src/Main.java
+++ ContainsDuplicate/src/Main.java
@@ -1,7 +1,24 @@
+import java.util.HashSet;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        
+
+        int[] nums = {2,2,3,4};
+        System.out.println(containsDuplicate(nums));
+
+    }
+
+    public static boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int n : nums) {
+            if (set.contains(n)) {
+                return true;
+            }
+            set.add(n);
+        }
+        return false;
     }
 }
