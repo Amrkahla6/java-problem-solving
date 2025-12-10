ecc39d7d7784051b9ea5355dba835b88bf0df15c
Amr Kahla <amrkahla6@gmail.com>
2025-12-10 16:07:25 +0200

    70. Climbing Stairs

---
+++ 5-Climbing Stairs/src/Main.java
@@ -0,0 +1,22 @@
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(climbStairs(7));
+    }
+
+    public static int climbStairs(int n) {
+        if (n <= 2) return n;
+
+        int oneStepBefore = 2;
+        int twoStepsBefore = 1;
+        int current = 0;
+
+        for (int i = 3; i <= n; i++) {
+            current = oneStepBefore + twoStepsBefore;
+            twoStepsBefore = oneStepBefore;
+            oneStepBefore = current;
+        }
+
+        return current;
+    }
+}
