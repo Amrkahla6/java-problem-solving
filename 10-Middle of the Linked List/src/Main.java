d2929697e4ca075fb195bc91a0ec9561d78e712b
Amr Kahla <amrkahla6@gmail.com>
2025-12-26 23:29:31 +0200

    Middle of the Linked List

---
+++ 10-Middle of the Linked List/src/Main.java
@@ -0,0 +1,24 @@
+
+public class Main {
+    public static void main(String[] args) {
+        ListNode head  = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+
+        ListNode middle = middleNode(head);
+        System.out.println(middle.val);
+    }
+
+    public static ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+}
